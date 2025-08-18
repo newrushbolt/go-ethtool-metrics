79fd09a8b2616ac6144492f64e34e3fe9f390d35
Sergei Mikhailov <mihailov.s@outlook.com>
2025-08-18 14:29:38 +0200

    fix: add per-queue drops

--- pkg/metrics/statistics/statistics_structs.go
+++ pkg/metrics/statistics/statistics_structs.go
@@ -25,14 +25,17 @@ type StatisticsInfo struct {
 
 type PerQueueStatistics []QueueStatistics
 type QueueStatistics struct {
-	TxBytes      *float64 `queue_statistics:"tx_bytes"`
-	RxBytes      *float64 `queue_statistics:"rx_bytes"`
+	TxBytes *float64 `queue_statistics:"tx_bytes"`
+	RxBytes *float64 `queue_statistics:"rx_bytes"`
+
 	RxUcastBytes *float64 `queue_statistics:"rx_ucast_bytes"`
 	RxMcastBytes *float64 `queue_statistics:"rx_mcast_bytes"`
 	RxBcastBytes *float64 `queue_statistics:"rx_bcast_bytes"`
 	TxUcastBytes *float64 `queue_statistics:"tx_ucast_bytes"`
 	TxMcastBytes *float64 `queue_statistics:"tx_mcast_bytes"`
 	TxBcastBytes *float64 `queue_statistics:"tx_bcast_bytes"`
+
+	RxDrops *float64 `queue_statistics:"rx_drops"`
 	// Other "yet to be supported metrics"
 	// First prio
 	// [0]: tx_errors: 0
@@ -72,24 +75,6 @@ type GeneralStatistics struct {
 	RxCrcErrors  *float64 `general_statistics:"rx_crc_errors,rx_crc_errors.nic"` // Only exists in Intel
 }
 
-// Other possible variables
-// TODO: Add testdata for "virtio"
-
-// rx_queue_0_packets: 99116794
-// rx_queue_0_bytes: 708784361629
-// rx_queue_0_drops: 0
-// rx_queue_0_xdp_packets: 0
-// rx_queue_0_xdp_tx: 0
-// rx_queue_0_xdp_redirects: 0
-// rx_queue_0_xdp_drops: 0
-// rx_queue_0_kicks: 4060
-
-// tx_queue_0_packets: 91340287
-// tx_queue_0_bytes: 1484407675445
-// tx_queue_0_xdp_tx: 0
-// tx_queue_0_xdp_tx_drops: 0
-// tx_queue_0_kicks: 89560041
-
-// Only exists in bnxt, should probably have separate struct
+// TODO: should create separate struct for pause-related metrics
 // tx_pause_frames: 0
 // rx_pause_frames: 0
